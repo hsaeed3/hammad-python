04bb96aff2af4f16cd6fa58f70b15fa5b1e264ab
hsaeed3 <hammadaidev@gmail.com>
2025-06-11 23:08:52 -0700

    feat: add type and component modules with user-facing documentation

---
+++ hammad/components/__init__.py
@@ -0,0 +1,5 @@
+"""hammad.components
+
+Contains fully-featured, incredibly simple to use components
+that require no other dependencies to use. These modules are all
+very literally named, and are designed to be used as-is."""
---
+++ hammad/types/__init__.py
@@ -0,0 +1,4 @@
+"""hammad.types
+
+Contains a collection of types meant to be used for user-facing
+purposes."""
