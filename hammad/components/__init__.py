98149d19549696ca29223fe8146b8f86ba32526d
hsaeed3 <hammadaidev@gmail.com>
2025-06-12 19:11:46 -0700

    feat: add components module with general utilities and custom logger class

---
+++ hammad/components/__init__.py
@@ -0,0 +1,6 @@
+"""hammad.components
+
+Contains general & common items or patterns that are found throughout
+common python applications and packages. These components are all
+meant to be used by the user, and call all be used without the use
+of any other depedencies."""
---
+++ hammad/components/logger.py
@@ -0,0 +1,4 @@
+"""hammad.components.logger
+
+Contains the `Logger` class, an extended logger with custom
+`styling` through rich."""
