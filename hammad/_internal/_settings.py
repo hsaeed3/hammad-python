57c09067ac5e83a93f327e3f8bcc5f246ae192d1
hsaeed3 <hammadaidev@gmail.com>
2025-06-13 02:04:34 -0700

    feat(logging): add internal logging module with configurable logger and rich formatting

--- hammad/_internal/_settings.py
+++ hammad/_internal/_settings.py
@@ -4,7 +4,7 @@
 # this specific instance is not meant to be used
 # by users
 
-__all__ = ["_get_hammad_settings_manager"]
+__all__ = ["_get_internal_settings_manager"]
 
 
 class _Key(str):
@@ -17,22 +17,22 @@ class _Key(str):
         return super().__new__(cls, value)
 
 
-class _HammadSettingsManager:
+class _InternalSettingsManager:
     """Internal manager that handles the 'global' library settings
     during execution life-cycle."""
 
-    _manager: "_HammadSettingsManager | None" = None
+    _manager: "_InternalSettingsManager | None" = None
     _initialized: bool = False
 
-    def __new__(cls) -> "_HammadSettingsManager":
+    def __new__(cls) -> "_InternalSettingsManager":
         if cls._manager is None:
             cls._manager = super().__new__(cls)
         return cls._manager
 
     def __init__(self) -> None:
-        if not _HammadSettingsManager._initialized:
+        if not _InternalSettingsManager._initialized:
             self._config: dict[_Key, object] = {}
-            _HammadSettingsManager._initialized = True
+            _InternalSettingsManager._initialized = True
 
     def get_entry(self, key: str) -> object:
         """Gets an entry from the configuration."""
@@ -44,12 +44,20 @@ class _HammadSettingsManager:
         config_key = _Key(key)
         self._config[config_key] = value
 
+    def delete_entry(self, key: str) -> None:
+        """Deletes an entry from the configuration."""
+        config_key = _Key(key)
+        self._config.pop(config_key, None)
 
-def _get_hammad_settings_manager() -> _HammadSettingsManager:
-    """Returns the singleton instance of the `_HammadSettingsManager`
+    def configure_entries(self, entries: list[str, object]) -> None:
+        """Initializes fields for a list of entry names
+        in the configuration (Used by modules with multiple
+        internal settings)."""
+        for entry, value in entries.items():
+            self.set_entry(entry, value)
+
+
+def _get_internal_settings_manager() -> _InternalSettingsManager:
+    """Returns the singleton instance of the `_InternalSettingsManager`
     class."""
-    global _HAMMAD_SETTINGS_MANAGER
-
-    if not _HAMMAD_SETTINGS_MANAGER:
-        _HAMMAD_SETTINGS_MANAGER = _HammadSettingsManager()
-    return _HAMMAD_SETTINGS_MANAGER
+    return _InternalSettingsManager()
