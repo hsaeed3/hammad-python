461b5598ed71c6c0485108450f38afbbafdb3d0e
hsaeed3 <hammadaidev@gmail.com>
2025-06-23 15:52:59 -0700

    feat(types): add file handling types including File, Document, Image, and Audio; implement loading from URLs and paths, and introduce FileSource for metadata management

---
+++ hammad/types/__init__.py
@@ -0,0 +1,8 @@
+"""hammad.types
+
+Contains a collection of models and type-like objects that have been designed both for
+specific internal use as well as general purpose types. Many of these "types" are
+dedicated modules themselves and have full functionality.
+
+The `types` namepsace is simply used to define singular objects or structures.
+"""
