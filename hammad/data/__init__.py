03b24fb4b6d30e8cae3fe875da2d50ff31f42669
Hammad Saeed <hsaeed@csod.com>
2025-06-24 13:43:41 -0700

    CLEAN

--- hammad/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""hammad.data
-
-This is one of the largest 'sub-modules' within the package and contains
-things like file utilities and objects, data validatation, database tools
-and more."""
--- hammad/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""hammad.models
-
-Contains the `BasedModel` and `basefield` system, an 'opinionated' Pydantic model clone
-using `msgspec` for faster serialization and deserialization as well as providing a few other
-extended functionality such as model conversion, turning fields into models, and more."""
