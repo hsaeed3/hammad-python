0bb4cc44bfe05920cb1c0de31098a5a09b097e26
Hammad Saeed <hsaeed@csod.com>
2025-06-24 18:33:12 -0700

    feat(core): introduce core components including BasedModel, field definitions, and utility functions for dynamic model creation

---
+++ hammad/core/__init__.py
@@ -0,0 +1,6 @@
+"""hammad.core
+
+Contains either resources related to the core implementation of this
+package, or resources that are not submodule specific / at a very low
+level for user usage. Most of the modules meant to be used within this
+module are exported to the top level of the package."""
