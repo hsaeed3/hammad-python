65b7ffc7386965c800a4ca007c6036091e970c9d
hsaeed3 <hammadaidev@gmail.com>
2025-06-27 07:30:43 -0700

    fix(yaml): correct formatting in __dir__ and read_yaml_file functions for consistency and clarity

--- hammad/yaml/converters.py
+++ hammad/yaml/converters.py
@@ -3,15 +3,10 @@
 from msgspec.yaml import encode as encode_yaml, decode as decode_yaml
 from ..data.types import Configuration as Yaml
 
-__all__ = (
-    "encode_yaml",
-    "decode_yaml",
-    "read_yaml_file",
-    "Yaml"
-)
+__all__ = ("encode_yaml", "decode_yaml", "read_yaml_file", "Yaml")
 
 
-def read_yaml_file(path : str) -> Yaml:
+def read_yaml_file(path: str) -> Yaml:
     """Parses a YAML file to return a Configuration object.
     This utilizes the following file types:
 
