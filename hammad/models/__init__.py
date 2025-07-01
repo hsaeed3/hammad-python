2aceedf955fed02e5a6119babda4fb7a78afef22
Hammad Saeed <hsaeed@csod.com>
2025-06-30 19:26:04 -0700

    feat(models): introduce foundational model structure with base classes, field definitions, and utilities for enhanced data validation and serialization capabilities

---
+++ hammad/models/__init__.py
@@ -0,0 +1,5 @@
+"""hammad.models
+
+Contains **BOTH** resources contains predefined models or base class like
+models, as well as modules & utilities specifically for various interfaces
+of models such as `pydantic`."""
