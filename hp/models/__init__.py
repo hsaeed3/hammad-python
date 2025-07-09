89981d1d5080058e317c1e75920feb3f179953c1
hsaeed3 <hammadaidev@gmail.com>
2025-07-09 02:11:14 -0700

    feat(hp): add initial module structure for hammad package, including internal utilities and Pydantic model converters, enhancing data modeling capabilities

---
+++ hp/models/__init__.py
@@ -0,0 +1,5 @@
+"""hp.models
+
+Contains resources in reference to data modeling and utilizing these
+models, along with specific resources for libraries such as `pydantic` and
+`msgspec`."""
---
+++ hp/models/extensions/__init__.py
@@ -0,0 +1,4 @@
+"""hp.models.extensions
+
+Contains a collection of resources specifically for external schemas or
+definitions for models."""
