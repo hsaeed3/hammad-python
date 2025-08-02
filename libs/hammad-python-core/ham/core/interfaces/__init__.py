7262945970af45c4bb94a8bb710fdada01de3ab5
hsaeed3 <hammadaidev@gmail.com>
2025-08-02 14:23:37 -0700

    chore(⚛ core): create `ham.core.interfaces` as common namespace for "base" level resources

---
+++ libs/hammad-python-core/ham/core/interfaces/__init__.py
@@ -0,0 +1,10 @@
+"""ham.core.interfaces
+
+Contains various core level interfaces that can be applied
+to various use cases and implementations.
+
+These resources include things such as state machines, event
+handlers and more. These resources are used both internally
+within the various `hammad-python-...` modules as well as
+usable by users directly.
+"""
